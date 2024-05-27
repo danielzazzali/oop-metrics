c0a7972fd8fbc5446f5aba157a4b6df7181d2ab4
Daniel Zazzali <dazazzali@gmail.com>
2024-05-27 15:19:20 -0400

    Recursive measure of files inside subdirectories

--- metrics/collector.js
+++ metrics/collector.js
@@ -10,19 +10,36 @@ const collectMetrics = (dirPath) => {
     jsFiles.forEach(filePath => {
         const relativePath = path.relative(dirPath, filePath);
         const fileContent = fs.readFileSync(filePath, 'utf-8');
-        const directoryPath = path.dirname(relativePath);
-        if (!result[directoryPath]) {
-            result[directoryPath] = {};
+        const directoryPathParts = path.dirname(relativePath).split(path.sep);
+
+        let currentDirectory = result;
+        for (const part of directoryPathParts) {
+            if (!currentDirectory[part]) {
+                currentDirectory[part] = {};
+            }
+            currentDirectory = currentDirectory[part];
         }
-        result[directoryPath][path.basename(filePath)] = analyzeFile(fileContent);
+
+        currentDirectory[path.basename(filePath)] = analyzeFile(fileContent);
     });
 
-    result['Project total metrics'] = Object.values(result).flatMap(Object.values).reduce((acc, metrics) => {
-        for (const key in metrics) {
-            acc[key] = (acc[key] || 0) + metrics[key];
-        }
-        return acc;
-    }, {});
+    const sumMetrics = (obj) => {
+        return Object.entries(obj).reduce((acc, [key, value]) => {
+            if (key.endsWith('.js')) {
+                for (const metric in value) {
+                    acc[metric] = (acc[metric] || 0) + value[metric];
+                }
+            } else if (typeof value === 'object') {
+                const nestedMetrics = sumMetrics(value);
+                for (const metric in nestedMetrics) {
+                    acc[metric] = (acc[metric] || 0) + nestedMetrics[metric];
+                }
+            }
+            return acc;
+        }, {});
+    };
+
+    result['Project total metrics'] = sumMetrics(result);
 
     return result;
 };
